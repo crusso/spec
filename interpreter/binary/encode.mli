c7ad5dcc35e6acc476cf3058c77e7539c2b49099
Claudio Russo <crusso@microsoft.com>
2018-01-31 13:31:08 +0000

    ported encoder

--- interpreter/binary/encode.mli
+++ interpreter/binary/encode.mli
@@ -1,4 +1,15 @@
+(*IF-OCAML*)
 exception Code of Source.region * string
+(*ENDIF-OCAML*)
+(*F#
+module Errors = sig
+module Code =
+sig
+  exception Error of Source.region * string
+end
+end
+exception Code = Errors.Code.Error
+F#*)
 
 val version : int32
 val encode : Ast.module_ -> string

c4dbdab5655f933c42a1a987abda3219b972733a
Claudio Russo <crusso@microsoft.com>
2018-02-05 14:09:31 +0000

    ported import.ml(i)

--- interpreter/script/import.ml
+++ interpreter/script/import.ml
@@ -1,16 +1,54 @@
 open Source
 open Ast
 
+
+
+(*IF-OCAML*)
 module Unknown = Error.Make ()
 exception Unknown = Unknown.Error  (* indicates unknown import name *)
 
 module Registry = Map.Make(struct type t = Ast.name let compare = compare end)
+(*ENDIF-OCAML*)
+(*F#
+open FSharp.Compatibility.OCaml 
+module Errors = struct
+module Unknown =
+struct
+  exception Error of Source.region * string
+  let warn at m = prerr_endline (Source.string_of_region at ^ ": warning: " ^ m)
+  let error at m = raise (Error (at, m))
+end
+end
+open Errors
+exception Unknown = Unknown.Error
+
+
+module Registry =
+   struct
+        type 'a t = Map<Ast.name,'a>
+        let empty = Map.empty
+        let add = Map.add
+        let find = Map.find
+   end
+
+F#*)
+
+
+
+
+
+
 let registry = ref Registry.empty
 
 let register name lookup = registry := Registry.add name lookup !registry
 
+(*IF-OCAML*)
 let lookup (m : module_) (im : import) : Instance.extern =
-  let {module_name; item_name; idesc} = im.it in
+(*ENDIF-OCAML*)
+(*F#
+let lookup (m : module_) (im : import) : Instance.``extern`` =
+F#*)
+  let {module_name=module_name; item_name=item_name; idesc=idesc} = im.it in
   let t = import_type m im in
   try Registry.find module_name !registry item_name t with Not_found ->
     Unknown.error im.at
--- interpreter/script/import.mli
+++ interpreter/script/import.mli
@@ -1,3 +1,4 @@
+(*IF-OCAML*)
 exception Unknown of Source.region * string
 
 val link : Ast.module_ -> Instance.extern list (* raises Unknown *)
@@ -6,3 +7,24 @@ val register :
   Ast.name ->
   (Ast.name -> Types.extern_type -> Instance.extern (* raises Not_found *)) ->
   unit
+(*ENDIF-OCAML*)
+(*F#
+
+
+
+module Errors = sig
+module Unknown =
+sig
+  exception Error of Source.region * string
+end
+end
+exception Unknown = Errors.Unknown.Error
+
+val link : Ast.module_ -> Instance.``extern`` list (* raises Unknown *)
+
+val register :
+  Ast.name ->
+  (Ast.name -> Types.extern_type -> Instance.``extern`` (* raises Not_found *)) ->
+  unit
+F#*)
+
